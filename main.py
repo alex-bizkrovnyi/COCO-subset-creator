83255ed4074ca6bb2a8bb55cd5afae460387a86b
Oleksandr_Byzkrovnyi <oleksandr.byzkrovnyi@nure.ua>
2023-12-14 13:10:53 +0200

    Move Main method to main.py file

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+from dataset_creation.dataset_creator import DatasetCreator
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    subset_classes = [
+        ['person', 'bicycle'],
+        ['person', 'dog'],
+        ['person', 'horse'],
+        ['person', 'cow'],
+        ['person', 'backpack'],
+        ['person', 'umbrella'],
+        ['person', 'handbag'],
+        ['person', 'frisbee'],
+        ['person', 'sports ball'],
+        ['person', 'kite'],
+        ['person', 'skateboard'],
+        ['frisbee'],
+        ['skateboard'],
+        ['kite'],
+        ['sports ball'],
+        ['person'],
+        ['bear'],
+        ['zebra'],
+        ['giraffe'],
+        ['horse'],
+        ['sheep'],
+        ['cow'],
+        ['elephant'],
+        ['cat']
+    ]
+    coco_train_annotation_file = 'COCO_DATASET_FULL_2017/annotations_trainval2017/annotations/instances_train2017.json'
+    coco_val_annotation_file = 'COCO_DATASET_FULL_2017/annotations_trainval2017/annotations/instances_val2017.json'
+    coco_train_images_zip_file = 'COCO_DATASET_FULL_2017/train2017.zip'
+    coco_valid_images_zip_file = 'COCO_DATASET_FULL_2017/val2017.zip'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    params = {
+        'train_source_annotations_path': coco_train_annotation_file,
+        'valid_source_annotations_path': coco_val_annotation_file,
+        'classes': subset_classes,
+        'train_source_images_zip_path': coco_train_images_zip_file,
+        'valid_source_images_zip_path': coco_valid_images_zip_file
+    }
+
+    DatasetCreator().create_dataset_subset(params)
